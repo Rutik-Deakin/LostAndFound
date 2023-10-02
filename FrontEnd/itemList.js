a6855e113e6b90151fb8c11a1cdf83e7be61b3b6
RUTIK TANK <tankrutik2009@gmail.com>
2023-10-02 22:23:22 +1100

    Add items integration

---
+++ FrontEnd/itemList.js
@@ -0,0 +1,38 @@
+let items = [];
+const fetchAllItems = async () => {
+    const response = await axios.get('http://localhost:3000/items')
+    console.log("Test: ", response.data.data);
+    items = response.data.data;
+
+function createItemCard(item) {
+    const itemCard = document.createElement('div');
+    itemCard.className = 'col-md-4';
+    itemCard.innerHTML = `
+      <div class="item-card">
+        <img src="${item?.image}" alt="Item Image">
+        <div class="p-3">
+          <h5>${item.title}</h5>
+          <small>${item?.dateAndTime}</small>
+          <p>${item.description}</p>
+          <p><strong>Location:</strong> ${item?.location}</p>
+        </div>
+      </div>
+    `;
+    return itemCard;
+  }
+  
+  // Function to append item cards to the 'items-area'
+  function appendItemCardsToPage(items) {
+    const itemsArea = document.getElementById('items-area');
+    items.forEach(item => {
+      const itemCard = createItemCard(item);
+      itemsArea.appendChild(itemCard);
+    });
+  }
+
+
+  
+  appendItemCardsToPage(items);
+  
+}
+fetchAllItems()
