083caa716964714227d3f8eb64e999191fd88438
tanzimtipu <tanzimtipu@gmail.com>
2023-10-05 17:48:09 +1100

    code-fix-for-itemlist-and-sign

--- FrontEnd/itemList.js
+++ FrontEnd/itemList.js
@@ -1,37 +1,34 @@
 let items = [];
 const fetchAllItems = async () => {
-    const response = await axios.get('http://localhost:3000/items')
-    console.log("Test: ", response.data.data);
-    items = response.data.data;
+  const response = await axios.get("http://localhost:3000/items");
+  console.log("Test: ", response.data.data);
+  items = response.data.data;
 
-createItemCard = (item) => {
-    const itemCard = document.createElement('div');
-    itemCard.className = 'col-md-4';
+  createItemCard = (item) => {
+    const itemCard = document.createElement("div");
+    itemCard.className = "col-md-4";
     itemCard.innerHTML = `
       <div class="item-card">
         <img src="${item?.image}" alt="Item Image">
         <div class="p-3">
           <h5>${item.title}</h5>
-          <small>${item?.dateAndTime}</small>
+          <small>${item?.date}</small>
           <p>${item.description}</p>
           <p><strong>Location:</strong> ${item?.location}</p>
         </div>
       </div>
     `;
     return itemCard;
-  }
-  
+  };
+
   appendItemCardsToPage = (items) => {
-    const itemsArea = document.getElementById('items-area');
-    items.forEach(item => {
+    const itemsArea = document.getElementById("items-area");
+    items.forEach((item) => {
       const itemCard = createItemCard(item);
       itemsArea.appendChild(itemCard);
     });
-  }
+  };
 
-
-  
   appendItemCardsToPage(items);
-  
-}
-fetchAllItems()
+};
+fetchAllItems();
